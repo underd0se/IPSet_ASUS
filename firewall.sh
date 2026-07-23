1e2cdd999695e39b7264273a4c02a1d9f069d5d8
underd0se <baris.erdogan@gmail.com>
2026-07-23 08:54:25 +0300

    feat: implement fork suffix versioning and update Filter_Version regex

--- firewall.sh
+++ firewall.sh
@@ -10,7 +10,7 @@
 #                                                                                                           #
 #                                 Router Firewall And Security Enhancements                                 #
 #                      By Adamm (Forked by underd0se) -  https://github.com/underd0se/Skynet-Zero           #
-#                                      20/07/2026 - v8.1.1 (Zero Swap)                                      #
+#                                  23/07/2026 - v8.1.1-sz.2-dev (Zero Swap)                                 #
 #############################################################################################################
 # amtm NoMD5check
 
@@ -1076,7 +1076,7 @@ Command_Not_Recognized() {
 }
 
 Filter_Version() {
-	grep -m1 -oE 'v[0-9]{1,2}([.][0-9]{1,2})([.][0-9]{1,2})'
+	grep -m1 -oE 'v[0-9]{1,2}([.][0-9]{1,2})([.][0-9]{1,2})([-a-zA-Z0-9.]+)?'
 }
 
 Filter_Date() {
